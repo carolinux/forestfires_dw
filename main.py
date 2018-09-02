3e0ae0479fd2b6a96efd368ff4210aa3a0b3ef06
Karolina <karolina.alexiou@siroop.ch>
2018-09-02 22:56:28 +0200

    minor edit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     directory_to_process = sys.argv[1]
     target_table_name = sys.argv[2]
     i = 0
-    for file in os.listdir(directory_to_process ):
+    for file in os.listdir(directory_to_process):
         if file.endswith(".hdf"):
             hdf_file = os.path.join(directory_to_process , file)
             i+=1
