073bf58ba3d312166504b83de3b699eb8c3b03f1
Karolina <karolina.alexiou@siroop.ch>
2018-09-07 16:08:11 +0200

    make results dir, re-indent

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ CREATE INDEX {}_geom_idx
     conn.close()
 
 
-
 def download_all_the_files(downloads_file, target_directory, username, password):
     if not os.path.exists(target_directory):
         os.mkdir(target_directory)
@@ -36,12 +35,13 @@ def download_all_the_files(downloads_file, target_directory, username, password)
         for i, line in enumerate(f):
             url = line.strip()
             target_file = os.path.join(target_directory, '{}.hdf'.format(i))
-            if os.path.exists(target_file) and os.path.getsize(target_file)>0:
+            if os.path.exists(target_file) and os.path.getsize(target_file) > 0:
                 continue
             else:
-                command = "wget -O {} --retry-connrefused --tries=20 --user {} --password {} {}".format(target_file, username, password, url)
+                command = "wget -O {} --retry-connrefused --tries=20 --user {} --password {} {}".format(target_file,
+                                                                                                        username,
+                                                                                                        password, url)
                 os.system(command)
-
 
 
 def sanity_check(year, directory):
@@ -58,26 +58,24 @@ def sanity_check(year, directory):
 if __name__ == "__main__":
 
     year = sys.argv[1]
-    username = sys.argv[2] # the username for NASA website
-    pw = sys.argv[3] # the password for NASA website
-    #db_user = sys.argv[4] # the db user
+    username = sys.argv[2]  # the username for NASA website
+    pw = sys.argv[3]  # the password for NASA website
+    # db_user = sys.argv[4] # the db user
     downloads_file = 'granules/{}.txt'.format(year)
 
     directory_to_process = year
-    download_all_the_files(downloads_file, directory_to_process, username, password)
+    download_all_the_files(downloads_file, directory_to_process, username, pw)
     sanity_check(year, directory_to_process)
-
 
     target_table_name = 'forestboxes{}'.format(year)
     i = 0
 
     create_table_for_forest_boxes(target_table_name)
 
-
     for file in os.listdir(directory_to_process):
         if file.endswith(".hdf"):
-            hdf_file = os.path.join(directory_to_process , file)
-            i+=1
+            hdf_file = os.path.join(directory_to_process, file)
+            i += 1
             print("processing file {}".format(i))
             process_file.process(hdf_file, target_table_name)
 
@@ -86,4 +84,3 @@ if __name__ == "__main__":
     # now: join with fire data
 
     process_fire.process(year)
-
--- process_fire.py
+++ process_fire.py
@@ -5,47 +5,47 @@ import psycopg2
 
 
 def process(year):
-   table_name = 'fires{}'.format(year)
-   boxes_table_name = 'forestboxes{}'.format(year)
-   forest_table_name = 'forest' + table_name
-   fn = "fire_csvs/df_cleaned_{}.csv".format(year)
-   df = pd.read_csv(fn)
-   df.rename(columns={df.columns[0]: "index"}, inplace=True)
-   conn_string = "dbname='fires' user='carolinux'"
-   conn = psycopg2.connect(conn_string)
-   cur = conn.cursor()
-   cur.execute("""CREATE TABLE {}
+    table_name = 'fires{}'.format(year)
+    boxes_table_name = 'forestboxes{}'.format(year)
+    forest_table_name = 'forest' + table_name
+    fn = "fire_csvs/df_cleaned_{}.csv".format(year)
+    df = pd.read_csv(fn)
+    df.rename(columns={df.columns[0]: "index"}, inplace=True)
+    conn_string = "dbname='fires' user='carolinux'"
+    conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE {}
 (
   fire_pt geometry,
   index integer
 );""".format(table_name))
-   cur.execute("""
+    cur.execute("""
 CREATE INDEX {}_geom_idx
   ON {}
   USING gist
   (fire_pt);""".format(table_name, table_name))
 
+    for _, values in df.iterrows():
+        sql = 'INSERT INTO {} (fire_pt, index) VALUES (st_setsrid(st_makepoint({}, {}),4326), {})'.format(table_name,
+                                                                                                          values[
+                                                                                                              'longitude'],
+                                                                                                          values[
+                                                                                                              'latitude'],
+                                                                                                          values[
+                                                                                                              'index'])
+        cur.execute(sql)
+    conn.commit()
+    print('doing the join at {}'.format(datetime.now()))
+    sql_join = 'create table {} as (select index, fire_pt from {} join {} on st_coveredby(fire_pt, box))'.format(
+        forest_table_name, boxes_table_name, table_name)
+    cur.execute(sql_join)
+    conn.commit()
+    print('join finished at {}'.format(datetime.now()))
+    import pandas.io.sql as sqlio
+    dat = sqlio.read_sql_query('select index from ' + forest_table_name, conn)
+    ids = set(dat.values[:, 0])
+    df['fire_started_in_forest_area'] = df['index'].apply(lambda x: x in ids)
 
-   for _, values in df.iterrows():
-      sql = 'INSERT INTO {} (fire_pt, index) VALUES (st_setsrid(st_makepoint({}, {}),4326), {})'.format(table_name,
-                                                                                                        values[
-                                                                                                           'longitude'],
-                                                                                                        values[
-                                                                                                           'latitude'],
-                                                                                                        values['index'])
-      cur.execute(sql)
-   conn.commit()
-   print('doing the join at {}'.format(datetime.now()))
-   sql_join = 'create table {} as (select index, fire_pt from {} join {} on st_coveredby(fire_pt, box))'.format(
-      forest_table_name, boxes_table_name, table_name)
-   cur.execute(sql_join)
-   conn.commit()
-   print('join finished at {}'.format(datetime.now()))
-   import pandas.io.sql as sqlio
-   dat = sqlio.read_sql_query('select index from ' + forest_table_name, conn)
-   ids = set(dat.values[:, 0])
-   df['fire_started_in_forest_area'] = df['index'].apply(lambda x: x in ids)
-   df.to_csv('results/{}.csv'.format(year), index=False)
-
-
-
+    if not os.path.exists('results'):
+        os.mkdir('results')
+    df.to_csv('results/{}.csv'.format(year), index=False)
